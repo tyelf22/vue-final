22a1d3181ddd67fce064f8246a15bd907b93aa25
tyelf22 <tyelf22@hotmail.com>
2019-11-04 23:10:29 -0700

    added player images and axios

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        NBA TEAM BUILDER
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <app-players></app-players>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Players from './components/Players';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: 'App',
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    appPlayers: Players,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Players.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-card max-width="400" class="mt-10 ml-10">
+      <v-img height="300" src='https://nba-players.herokuapp.com/players/james/lebron'>
+
+      </v-img>
+        <v-card-title> {{ playerName }}</v-card-title>
+        <v-card-text> {{ playerTeam }} </v-card-text>
+        <v-card-text> {{ playerRating }} </v-card-text>
+        <v-btn @click="getData">Get data</v-btn>
+        <ul>
+          <li>
+            {{ player }}
+          </li>
+        </ul>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      playerName: "Lebron James",
+      playerTeam: "Los Angeles Lakers",
+      playerRating: 96,
+      player: null,
+    }
+  },
+  methods: {
+    getData() {
+    axios.get('https://www.balldontlie.io/api/v1/players?search=lebron')
+    .then(response => (this.player = response))
+    }
+
+  },
+
+}
+  
+
+</script>
+
+<style scoped>
+
+</style>
