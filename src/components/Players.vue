4ad0ddbe94d00a3bd7613b125a508b84afe7c716
tyelf22 <tyelf22@hotmail.com>
2019-11-06 13:39:32 -0700

    added array for adding players

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
-    <v-card max-width="400" class="mt-10 ml-10">
-      <v-card-title>Type the name of the player</v-card-title>
+  <div class="players">
+    <!-- <v-card max-width="400" class="mt-10 ml-10"> -->
+        <h1>Type the name of the player</h1> 
         <v-text-field v-model="inputF" label="first">{{ inputF }}</v-text-field>
         <v-text-field v-model="inputL" label="last">{{ inputL }}</v-text-field>
         <v-btn @click="getData">Get data</v-btn>
        
           <h1>{{ firstName }} {{lastName}} </h1>
           <h3>{{ team }}</h3>
-    
-    </v-card>
 
+          <v-btn @click="toArray">Save Team</v-btn>
 
+          <ul>
+            <li v-for="playerFirst in playerFirstNames" :key="playerFirst.id"> {{ playerFirst }}</li>
+          </ul>
   </div>
 </template>
 
@@ -26,24 +28,36 @@ export default {
       inputF: '',
       inputL: '',
       team: '',
+      playerFirstNames: [],
+      playerLastNames: [],
+      playerTeams: []
     }
   },
   methods: {
     getData() {
     
-       axios.get("https://www.balldontlie.io/api/v1/players?search=" + this.inputF + " " + this.inputL)
+        axios.get("https://www.balldontlie.io/api/v1/players?search=" + this.inputF + " " + this.inputL)
        .then(response => this.firstName = response.data.data[0].first_name)
 
        axios.get("https://www.balldontlie.io/api/v1/players?search=" + this.inputF + " " + this.inputL)
        .then(response => this.lastName = response.data.data[0].last_name) 
 
        axios.get("https://www.balldontlie.io/api/v1/players?search=" + this.inputF + " " + this.inputL)
-       .then(response => this.team = response.data.data[0].team.full_name)
-    
+       .then(response => this.team = response.data.data[0].team.full_name) 
+   
     },
+
+    toArray() {
+      this.playerFirstNames.push(this.firstName)
+      this.playerLastNames.push(this.lastName)
+      console.log(this.playerFirstNames); // eslint-disable-line no-console
+      console.log(this.playerLastNames); // eslint-disable-line no-console
+
+    }
 
 
   },
+
 
 }
   
@@ -51,5 +65,8 @@ export default {
 </script>
 
 <style scoped>
+.players {
+  margin: 50px 50px;
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router'
+import Players from './components/Players'
+import PlayerStats from './components/PlayerStats'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {path:'/', component: Players},
+    {path: '/playerstats', component: PlayerStats}
+  ]
+})
 
 Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
