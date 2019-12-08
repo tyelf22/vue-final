c975f273f63006579b921d0a665ac2f9f5d8895d
tyelf22 <tyelf22@hotmail.com>
2019-12-07 23:35:56 -0700

    comment clean up

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -27,34 +27,11 @@
         </v-row>
   
         <app-player-card></app-player-card>
-            
-<!--             <v-row justify="center">
-              <v-card color="primary" class="mt-5 ma-1" v-for="(player, index) in this.$store.state.playerNames" :key="player.id"> 
-                  <v-img 
-                    class="align-end"
-                    :src="player.image"
-                  >
-                    <v-card-title class="white--text">{{player.name}}</v-card-title>
-                  </v-img>
-                <v-row justify="space-between">
-            
-                <v-card-text class="white--text"> 
-                  Weight: {{ player.weight }} 
-                  <br>
-                  Height: {{ player.height }}
-                  </v-card-text>
-                  <v-btn fab small absolute bottom right class="mb-3 mr-n5" color="error" @click="deleteItem(index)">
-                    <v-icon>mdi-delete</v-icon>
-                  </v-btn>
-                </v-row>
-              </v-card>
-          </v-row> -->  
-         
 
-          <!-- Used custom directive -->
-          <v-alert v-fade:display.delayed="'none'" id="playerAlert" v-model="playerAlert" v-if="playerAlert" type="success"> {{this.firstName}} {{this.lastName}} added!</v-alert>
-          <v-alert v-fade:display.delayed="'none'" id="errorAlert" v-model="alert" v-if="alert" type="error">No player found</v-alert>
-          <v-alert v-fade:display.delayed="'none'" id="errorAlert" v-model="seasonAlert" v-if="seasonAlert" type="error">Choose a current player</v-alert>
+        <!-- Used custom directive -->
+        <v-alert v-fade:display.delayed="'none'" id="playerAlert" v-model="playerAlert" v-if="playerAlert" type="success"> {{this.firstName}} {{this.lastName}} added!</v-alert>
+        <v-alert v-fade:display.delayed="'none'" id="errorAlert" v-model="alert" v-if="alert" type="error">No player found</v-alert>
+        <v-alert v-fade:display.delayed="'none'" id="errorAlert" v-model="seasonAlert" v-if="seasonAlert" type="error">Choose a current player</v-alert>
    
   </div>
 </template>
